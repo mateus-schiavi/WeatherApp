4705580b830a9ad9948dd313d7867f53e8ce47c3
Mateus Schiavi <mtsschiavi@gmail.com>
2023-10-25 20:02:05 -0300

    fixed components

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,35 @@ import Right from './Components/Right/Right';
 function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [forecastData, setForecastData] = useState(null);
+  const [forecastDataFiveDays, setForecastDataFiveDays] = useState(null);
   const [searchCity, setSearchCity] = useState('Suzano');
   const apiKey = '9173b14f8f0405e63830c31b389b0bc0'; // Replace with your API key
 
   useEffect(() => {
     const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}&units=metric`;
     const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&appid=${apiKey}&units=metric`;
+    const forecastUrlFiveDays = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${searchCity}&appid=${apiKey}&units=metric&cnt=5`;
 
     const fetchData = async () => {
       try {
-        const [currentResponse, forecastResponse] = await Promise.all([
+        const [currentResponse, forecastResponse, forecastResponseFiveDays] = await Promise.all([
           fetch(currentWeatherUrl),
           fetch(forecastUrl),
+          fetch(forecastUrlFiveDays),
         ]);
 
         if (currentResponse.ok && forecastResponse.ok) {
           const currentData = await currentResponse.json();
           const forecastData = await forecastResponse.json();
+          const forecastDataFiveDays = await forecastResponseFiveDays.json();
 
           setWeatherData(currentData);
           setForecastData(forecastData);
+          setForecastDataFiveDays(forecastDataFiveDays);
 
           console.log('Current Weather Data:', currentData);
           console.log('Forecast Data:', forecastData);
+          console.log('Forecast Data for Five Days:', forecastDataFiveDays);
         } else {
           console.error('Error in API response');
         }
@@ -46,7 +52,11 @@ function App() {
         searchCity={searchCity}
         setSearchCity={setSearchCity}
       />
-      <Right weatherData={weatherData} forecastData={forecastData} />
+      <Right
+        weatherData={weatherData}
+        forecastData={forecastData}
+        forecastDataFiveDays={forecastDataFiveDays} // Pass the new state to the Right component
+      />
     </div>
   );
 }
--- src/Components/Right/Right.jsx
+++ src/Components/Right/Right.jsx
@@ -5,102 +5,98 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 const windDirectionMap = {
-    0: 'N',
-    45: 'NE',
-    90: 'E',
-    135: 'SE',
-    180: 'S',
-    225: 'SW',
-    270: 'W',
-    315: 'NW',
+  0: 'N',
+  45: 'NE',
+  90: 'E',
+  135: 'SE',
+  180: 'S',
+  225: 'SW',
+  270: 'W',
+  315: 'NW',
 };
 
 function Right({ weatherData, forecastData }) {
-    if (!weatherData || !forecastData) {
-        return (
-            <div className='right-container'>
-                <p>Loading...</p>
-            </div>
-        );
-    }
-
-    const getWindDirection = (degrees) => {
-        const closestDirection = Object.keys(windDirectionMap).reduce((prev, curr) =>
-            Math.abs(curr - degrees) < Math.abs(prev - degrees) ? curr : prev
-        );
-        return windDirectionMap[closestDirection];
-    };
-
-    const today = new Date();
-    const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const currentDay = today.getDay();
-
-    // Filter daily forecasts for the next five days
-    const dailyForecasts = forecastData.list.filter((forecast, index) => index % 8 === 0);
-
+  if (!weatherData || !forecastData) {
     return (
-        <div className='right-container'>
-            <div className='temperature-buttons'>
-                <button className='button-celsius'>°C</button>
-                <button className='button-fahrenheit'>°F</button>
-            </div>
-            <div className='weather-images'>
-                {dailyForecasts.map((dayData, index) => {
-
-                    const { temp_max: maxTemp, temp_min: minTemp } = dayData.main;
-
-                    // Calculate the date from the forecast data
-                    const forecastDate = new Date(dayData.dt * 1000); // Convert seconds to milliseconds
-
-                    const dayName = index === 0 ? 'Today' : dayNames[(currentDay + index) % 7];
-
-                    return (
-                        <div key={index} className='day-forecast'>
-                            <p>{dayName}</p>
-                            <img src={`https://openweathermap.org/img/wn/${dayData.weather[0].icon}.png`} alt={dayData.weather[0].description} />
-                            <div className='temperature-info'>
-                                <p>Max: {maxTemp.toFixed(0)}°C</p>
-                                <p>Min: {minTemp.toFixed(0)}°C</p>
-                            </div>
-                        </div>
-                    );
-                })}
-            </div>
-            <div className='weather-container'>
-                <div className='weather-info'>
-                    <div className='info-box wind-status'>
-                        <h3>Wind Status</h3>
-                        <p>{weatherData.wind.speed} mph</p>
-                        <FontAwesomeIcon icon={faArrowUp} rotation={weatherData.wind.deg} />
-                        {getWindDirection(weatherData.wind.deg)}
-                    </div>
-                    <div className='info-box humidity'>
-                        <h3>Humidity</h3>
-                        <p>{weatherData.main.humidity}%</p>
-                        <div className='progress-labels'>
-                            <span className="label-left">0%</span>
-                            <span className="label-middle">50%</span>
-                            <span className="label-right">100%</span>
-                        </div>
-                        <progress max="100" value={weatherData.main.humidity}></progress>
-                    </div>
-                    <div className='info-box visibility'>
-                        <h3>Visibility</h3>
-                        <p>{(weatherData.visibility / 1609.34).toFixed(2)} miles</p>
-                    </div>
-                    <div className='info-box air-pressure'>
-                        <h3>Air Pressure</h3>
-                        <p>{weatherData.main.pressure} mb</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+      <div className='right-container'>
+        <p>Loading...</p>
+      </div>
     );
+  }
+
+  const getWindDirection = (degrees) => {
+    const closestDirection = Object.keys(windDirectionMap).reduce((prev, curr) =>
+      Math.abs(curr - degrees) < Math.abs(prev - degrees) ? curr : prev
+    );
+    return windDirectionMap[closestDirection];
+  };
+
+  const today = new Date();
+  const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const currentDay = today.getDay();
+
+  // Filter daily forecasts for the next five days
+  const dailyForecasts = forecastData.list.filter((forecast, index) => index % 8 === 0);
+
+  return (
+    <div className='right-container'>
+      <div className='temperature-buttons'>
+        <button className='button-celsius'>°C</button>
+        <button className='button-fahrenheit'>°F</button>
+      </div>
+      <div className='weather-images'>
+        {dailyForecasts.map((dayData, index) => {
+          const { temp_max: temp_max, temp_min: temp_min } = dayData.main;
+          const forecastDate = new Date(dayData.dt * 1000); // Convert seconds to milliseconds
+          const dayName = index === 0 ? 'Tomorrow' : dayNames[(currentDay + index) % 7];
+          const weatherDescription = dayData.weather[0].description;
+          return (
+            <div key={index} className='day-forecast'>
+              <p>{dayName}</p>
+              <img src={`https://openweathermap.org/img/wn/${dayData.weather[0].icon}.png`} alt={dayData.weather[0].description} />
+              <div className='temperature-info'>
+                <p>Max: {temp_max.toFixed(0)}°C</p>
+                <p>Min: {temp_min.toFixed(0)}°C</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className='weather-container'>
+        <div className='weather-info'>
+          <div className='info-box wind-status'>
+            <h3>Wind Status</h3>
+            <p>{weatherData.wind.speed} mph</p>
+            <FontAwesomeIcon icon={faArrowUp} rotation={weatherData.wind.deg} />
+            {getWindDirection(weatherData.wind.deg)}
+          </div>
+          <div className='info-box humidity'>
+            <h3>Humidity</h3>
+            <p>{weatherData.main.humidity}%</p>
+            <div className='progress-labels'>
+              <span className="label-left">0%</span>
+              <span className="label-middle">50%</span>
+              <span className="label-right">100%</span>
+            </div>
+            <progress max="100" value={weatherData.main.humidity}></progress>
+          </div>
+          <div className='info-box visibility'>
+            <h3>Visibility</h3>
+            <p>{(weatherData.visibility / 1609.34).toFixed(2)} miles</p>
+          </div>
+          <div className='info-box air-pressure'>
+            <h3>Air Pressure</h3>
+            <p>{weatherData.main.pressure} mb</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 Right.propTypes = {
-    weatherData: PropTypes.object,
-    forecastData: PropTypes.object,
+  weatherData: PropTypes.object,
+  forecastData: PropTypes.object,
 };
 
 export default Right;
